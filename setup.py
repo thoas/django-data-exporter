39a909371ed01be36d730d1653b463887435bf8e
Florent Messa <florent.messa@gmail.com>
2012-09-26 11:20:32 +0200

    Add README to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
 version = __import__('data_exporter').__version__
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(root, 'README.rst')) as f:
+    README = f.read()
 
 setup(
     name='django-data-exporter',
     version=version,
     description='Export asynchronously your data from your Django models',
+    long_description=README,
     author='Florent Messa',
     author_email='florent.messa@gmail.com',
     url='http://github.com/thoas/django-data-exporter',
